17756aea9e80ff740da9acba29281907705aeac2
SaChan <vltpcks@gmail.com>
2022-12-29 21:26:51 +0900

    panel transform setting for js

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-let loadElem;
+let loaderElem;
+
+let panelIitemElem;
+let panelIistElem;
+
+let PanelNum = 10;
+let panelSize = 300;
+let uniteRadian = (2 * Math.PI) / PanelNum;
+let uniteDegree = 360 / PanelNum;
 
 function setElem() {
-  loadElem = document.querySelector(".loader-event");
+  loaderElem = document.querySelector(".loader");
+  panelIitemElem = document.querySelectorAll(".panel-item");
+  panelIistElem = document.querySelector(".panel-list");
+}
+
+function setPanelItem() {
+  const dist = panelSize / 2 / Math.tan(uniteRadian / 2) + panelSize * 0.65;
+  for (let i = 0; i < panelIitemElem.length; i++) {
+    panelIitemElem[i].style.transform = `rotateY(${
+      uniteDegree * i
+    }deg) translateZ(${-dist}px)`;
+  }
 }
 
 window.addEventListener("load", () => {
   setElem();
 
-  loadElem.addEventListener(    "transitionend", () => {
+  loaderElem.addEventListener("transitionend", function () {
     this.remove();
   });
 
   document.body.classList.remove("loader-event");
+
+  setPanelItem();
 });
