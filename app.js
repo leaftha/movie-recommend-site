d1932618322d6f35c85f46e1694ff17321120e49
SaChan <vltpcks@gmail.com>
2022-12-30 16:43:57 +0900

    Cover Project setting to html css and slider setting

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import { data } from "./data";
+
 let loaderElem;
 
 let panelIitemElem;
---
+++ data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    title: "Parasite",
+    slide1: "./image/slide/parasite/1.jpg",
+    slide2: "./image/slide/parasite/2.jpg",
+    slide3: "./image/slide/parasite/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Joker",
+    slide1: "./image/slide/joker/1.jpg",
+    slide2: "./image/slide/joker/2.jpg",
+    slide3: "./image/slide/joker/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "The greatest showman",
+    slide1: "./image/slide/showman/1.jpg",
+    slide2: "./image/slide/showman/2.jpg",
+    slide3: "./image/slide/showman/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Avatar",
+    slide1: "./image/slide/avatar/1.jpg",
+    slide2: "./image/slide/avatar/2.jpg",
+    slide3: "./image/slide/avatar/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "LaLa land",
+    slide1: "./image/slide/lala/1.jpg",
+    slide2: "./image/slide/lala/2.jpg",
+    slide3: "./image/slide/lala/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Your name",
+    slide1: "./image/slide/name/1.jpg",
+    slide2: "./image/slide/name/2.jpg",
+    slide3: "./image/slide/name/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Saving Private Ryan",
+    slide1: "./image/slide/ryan/1.jpg",
+    slide2: "./image/slide/ryan/2.jpg",
+    slide3: "./image/slide/ryan/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "The Shawshank Redemption",
+    slide1: "./image/slide/shawshank/1.jpg",
+    slide2: "./image/slide/shawshank/2.jpg",
+    slide3: "./image/slide/shawshank/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Titanic",
+    slide1: "./image/slide/titanic/1.jpg",
+    slide2: "./image/slide/titanic/2.jpg",
+    slide3: "./image/slide/titanic/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+  {
+    title: "Spirited Away",
+    slide1: "./image/slide/spirited/1.jpg",
+    slide2: "./image/slide/spirited/2.jpg",
+    slide3: "./image/slide/spirited/3.jpg",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerumearum, eius deleniti doloribus perspiciatis doloremque nesciunteos beatae eaque nemo! Placeat quas eius animi dignissimossapiente voluptate veniam vitae quae suscipit vero voluptatibusmagnam in aliquid molestiae non ducimus saepe nostrum dicta velconsequuntur iure, molestias labore. Accusantium providenteligendi vitae alias explicabo, optio nulla illum minima nihillaborum labore quasi molestias obcaecati deserunt esse velit fugaiste asperiores qui fugit officiis culpa delectus rerum.Aspernatur quas similique veniam blanditiis doloremque deseruntqui doloribus voluptates asperiores, consequatur magni omnis sitreiciendis nesciunt dolores quisquam id quos suscipit. Eos distinctio facilis officia dolore vitae eum quis, numquam magniodio ratione in delectus architecto exercitationem est doloremqueinventore porro reiciendis commodi corrupti sit nam. Eveniet,commodi praesentium? Illum, doloribus repudiandae eos odio quosofficiis voluptates laborum adipisci nihil non! Animi nihil",
+  },
+];
